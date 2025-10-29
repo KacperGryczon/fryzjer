003c7284b31aaf460360e0ae1585563c6d78536f
KacperGryczon <kacper.gryczon@gmail.com>
2025-10-29 20:45:18 +0100

    test

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  sidebar.classList.add("active");
+}
+
+function closeSidebar() {
+  sidebar.classList.remove("active");
+}
